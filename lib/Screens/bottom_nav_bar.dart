6238cfd95f641ddb13bfb04cd628c91dbf674429
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-06-27 19:49:35 +0530

    Changed the design of the bottom nav bar

--- lib/Screens/bottom_nav_bar.dart
+++ lib/Screens/bottom_nav_bar.dart
@@ -11,29 +11,52 @@ class _BottomNavbarState extends State<BottomNavbar> {
   int currentIndex = 0;
 
   List<Widget> body = [
-    const Text('Delivery'),
-    const Text('History'),
-    const Text('Money'),
+    const Center(child: Text('Delivery')),
+    const Center(child: Text('History')),
+    const Center(child: Text('Money')),
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: body[currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.delivery_dining),
-            label: 'Delivery',
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          splashFactory: NoSplash.splashFactory,
+          highlightColor: Colors.transparent,
+        ),
+        child: BottomNavigationBar(
+          currentIndex: currentIndex,
+          onTap: (value) {
+            setState(() {
+              currentIndex = value;
+            });
+          },
+          iconSize: 30,
+          unselectedItemColor: Colors.grey,
+          unselectedFontSize: 14,
+          selectedLabelStyle: const TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w600,
+            color: Colors.black,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.receipt_long_rounded),
-            label: 'History',
+          selectedIconTheme: const IconThemeData(
+            color: Color.fromARGB(255, 223, 19, 4),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.wallet_rounded),
-            label: 'Money',
-          ),
-        ],
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.delivery_dining),
+              label: 'Delivery',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.receipt_long_rounded),
+              label: 'History',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.wallet_rounded),
+              label: 'Money',
+            ),
+          ],
+        ),
       ),
     );
   }
