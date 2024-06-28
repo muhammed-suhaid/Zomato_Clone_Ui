1f92bfe04c48faafb99e2d3c835fa085533a5264
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-06-28 14:31:49 +0530

    Created category data

---
+++ lib/data/category_data.dart
@@ -0,0 +1,53 @@
+import 'package:zomato_clone_ui/data/image_data.dart';
+import 'package:zomato_clone_ui/models/category_model.dart';
+
+List categories = [
+  Category(
+    image: biriyani,
+    name: 'Biriyani',
+  ),
+  Category(
+    image: burger,
+    name: 'Burger',
+  ),
+  Category(
+    image: cake,
+    name: 'Cake',
+  ),
+  Category(
+    image: chicken,
+    name: 'Chicken',
+  ),
+  Category(
+    image: chineese,
+    name: 'Chineese',
+  ),
+  Category(
+    image: falooda,
+    name: 'Falooda',
+  ),
+  Category(
+    image: fries,
+    name: 'Fries',
+  ),
+  Category(
+    image: iceCream,
+    name: 'Ice Cream',
+  ),
+  Category(
+    image: juice,
+    name: 'Juice',
+  ),
+  Category(
+    image: parotta,
+    name: 'Parotta',
+  ),
+  Category(
+    image: pizza,
+    name: 'Pizza',
+  ),
+  Category(
+    image: shawarma,
+    name: 'Shawarma',
+  ),
+];
