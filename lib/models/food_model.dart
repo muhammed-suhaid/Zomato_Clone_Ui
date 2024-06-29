aa9b949aed5e99fdfa20271c86b68902512f1c46
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-06-29 22:44:46 +0530

    Created a model for food

---
+++ lib/models/food_model.dart
@@ -0,0 +1,15 @@
+class Food {
+  final String restaurantName;
+  final String foodName;
+  final String price;
+  final String place;
+  final String rating;
+
+  Food({
+    required this.restaurantName,
+    required this.foodName,
+    required this.price,
+    required this.place,
+    required this.rating,
+  });
+}
